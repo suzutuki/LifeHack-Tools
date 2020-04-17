749c430df433e91d867f907263131752e0300725
suzutuki <renath31@gmial.com>
2020-04-17 02:28:29 +0000

    second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,17 +2,15 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  
   def new
     @task = Task.new
   end
+  
   def edit
     @task = Task.find(params[:id])
   end
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to root_url
-  end
+  
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -21,6 +19,22 @@ class TasksController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_url
+    else
+    render 'edit'
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_url
+  end
+  
   def toggle
     head :no_content
     @task = Task.find(params[:id])
@@ -28,7 +42,7 @@ class TasksController < ApplicationController
     @task.save
   end
   private
-    def tasks_params
+    def task_params
       params[:task].permit(:title)
     end
 end
