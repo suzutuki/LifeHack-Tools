86d0b2c29f486cd4bfc9e800f4df32ccb940e6bd
EC2 Default User <ec2-user@ip-172-31-47-89.ap-northeast-1.compute.internal>
2020-04-21 10:27:48 +0000

    routes create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
   root 'tasks#index'
+  get '/signup', to: 'users#new'
+  resources :tasks
   post '/tasks/:id/toggle' => 'tasks#toggle'
 end
