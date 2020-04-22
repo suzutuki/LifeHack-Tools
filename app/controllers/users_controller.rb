51bcb0a3d194278ef20a25bd4dbeb55e7089c9eb
EC2 Default User <ec2-user@ip-172-31-47-89.ap-northeast-1.compute.internal>
2020-04-22 04:18:51 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find_by(params[:id])
   end
+  def new
+    @user = User.new
+  end
+  def destroy
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録に成功しました!"
+      redirect_to @user
+    else
+    render 'new'
+    end
+  end
+  
+    
+  
+  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email,:password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   root 'tasks#index'
-  get '/signup', to: 'users#new'
+  get 'users/new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :tasks
+  resources :users
   post '/tasks/:id/toggle' => 'tasks#toggle'
 end
