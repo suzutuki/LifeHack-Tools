79f855d29d4d3202bbc6bdbc2f44da12685bb0e3
akina <bokou31@gmail.com>
2020-08-11 18:41:59 +0900

    fix Ajax

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,6 @@ class Task < ApplicationRecord
             uniqueness: {message: 'すでに設定したリストと優先順位がかぶっています'}
   validates :title, presence: {message: 'タイトルを入力してください'},
             length: {minimum: 2, message: '2文字以上で入力してください'}
-  validates :content, length: {maximum: 50, message: '50文字まで登録できます。'}
+  validates :content, length: {maximum: 50, message: '登録できるのは50文字までです'}
 
 end
