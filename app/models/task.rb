463c66bbdb725f658ee4b0db91e2dec45375dba4
akina <bokou31@gmail.com>
2020-08-11 16:06:39 +0900

    add checkbox.scss fix scss

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user
   default_scope -> { order(priority: :asc) }
-  validates :priority, presence: { message: '数値を入力してください！'}
-
-  validates :title,   presence: { message: 'タイトルを入力してください!'},
-             length: {minimum: 2, message: '2文字以上で入力してください！'}
+  validates :priority, numericality: {message: '数値を入力してください！'},
+            uniqueness: {message: 'すでに設定したリストと優先順位がかぶっています'}
+  validates :title, presence: {message: 'タイトルを入力してください'},
+            length: {minimum: 2, message: '2文字以上で入力してください'}
   validates :content, length: {maximum: 50, message: '50文字まで登録できます。'}
+
 end
