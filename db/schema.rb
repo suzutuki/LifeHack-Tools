5e5fe88f90d959b8202993904c38241c6a4bdb24
akina <bokou31@gmail.com>
2021-03-23 09:53:09 +0900

    fixed pagination.scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_210401) do
+ActiveRecord::Schema.define(version: 2021_03_22_231730) do
 
   create_table "its", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_03_22_210401) do
     t.float "sport_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "start_on"
+    t.float "end_on"
     t.index ["user_id"], name: "index_sports_on_user_id"
   end
 
--- test/models/sport_test.rb
+++ test/models/sport_test.rb
@@ -3,8 +3,10 @@
 # Table name: sports
 #
 #  id         :bigint           not null, primary key
+#  end_on     :float(24)
 #  sport_day  :date
 #  sport_time :float(24)
+#  start_on   :float(24)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint
