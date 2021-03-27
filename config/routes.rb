753c54bb395dd8b871e2c536aa30dea691fcff41
akina <bokou31@gmail.com>
2021-03-27 13:24:48 +0900

    fix tasks/index

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,7 @@ Rails.application.routes.draw do
   resources :hiits
   resources :sports
   root 'users#index'
-  get 'simple', to: 'steps#simple'
-  get 'sample', to: 'lists#sample'
-  get 'swipe', to: 'lists#swipe'
   resources :lists
-
-  resources :steps do
-    collection do
-      delete 'destroy_all'
-    end
-  end
-  post 'goal_create', to: 'steps#goal_create'
-  get 'goal', to: 'steps#goal'
-  get 'example', to: 'steps#example'
   resources :its do
     collection do
       delete 'destroy_all'
