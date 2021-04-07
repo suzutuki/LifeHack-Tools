e1c411b39695d96347ee03be9534d0255a2761c3
akina <bokou31@gmail.com>
2021-04-08 04:55:19 +0900

    add config

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,44 +15,40 @@ class ImageUploader < CarrierWave::Uploader::Base
   # アップロードされたファイルが保存されるディレクトリを上書きします。
   # これは、マウントされることを前提としたアップローダのための、賢明なデフォルトです。
   # 画像ごとに保存するディレクトリを変える
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # 許可する画像の拡張子
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
 
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.#{file.extension}" if original_filename
+  end
   # ファイルがアップロードされていない場合には、デフォルトのURLを提供する。
   # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  # Rails 3.1+ のアセットパイプラインとの互換性のため。
+  # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
+  # アップロードされたファイルを処理します。
   # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
+  # アップロードされたファイルの異なるバージョンを作成します。
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # アップロードされるファイルのファイル名を変更します。
+  # 詳細は uploader/store.rb を参照してください。
   # def filename
   #   "something.jpg" if original_filename
   # end
