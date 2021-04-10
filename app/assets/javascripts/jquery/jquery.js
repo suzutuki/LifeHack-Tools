975f673bd123cad3827ea9ed2ae0906d4fcd7df1
akina <bokou31@gmail.com>
2021-04-10 15:59:46 +0900

    Refactoring

--- app/assets/javascripts/jquery/jquery.js
+++ app/assets/javascripts/jquery/jquery.js
@@ -1,25 +1,22 @@
-// ボタンが押されたら優先順位,1位以外を非表示にする。not-priority-1がターゲット
+// ボタンが押されたら優先順位,1位以外を非表示にする。itemnot-priority-1がターゲット
 $(function () {
     $("trigger_priority").click(function () {
         $(".itemnot-priority-1").toggle("slow");
     });
 });
-// ボタンが押されたら更新・作成日時を非表示にする。tasks-targetがターゲット
-$(function () {
-    $("trigger_time item").click(function () {
-        $(".tasks-time").toggle("slow");
-    });
-});
+
 // フラッシュメッセージのアニメーション
 $(function () {
     setTimeout("$('.time-limit').fadeOut('slow')", 6000)
 });
 
+// ボタンが押されたら更新・作成日時を非表示にする。tasks-targetがターゲット
 $(function () {
     $("trigger_time").click(function () {
-        $(".tasks-time").toggle("slow");
+        $(".tasks__time").toggle("slow");
     });
 });
+
 // ToDoリストでチェックボタンを動的(チェックされた、またはされてない状態を保存)にする
 $(function () {
     $('input:checkbox[name="task"]').click(function () {
