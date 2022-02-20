d185d4c3869914ac3735c534d50993e355085ec0
suzutuki <61527476+suzutuki@users.noreply.github.com>
2022-02-21 06:19:16 +0900

    Update puma.rb
    推奨の設定に変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,9 @@
-workers Integer(ENV["WEB_CONCURRENCY"] || 2)
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-preload_app!
-
-rackup DefaultRackup
-# port  ENV['PORT'] || 3000
+# portENV['PORT']|| 3000
 bind "unix:///var/www/LifeHack-Tools/tmp/sockets/puma.sock"
+environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 plugin :tmp_restart
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
