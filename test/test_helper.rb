c5d96e9b275e51baa0d66c7c268020fd940103c7
suzutuki <61527476+suzutuki@users.noreply.github.com>
2022-08-29 03:31:08 +0900

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,11 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  
   # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
   # Add more helper methods to be used by all tests here...
-  
   # テストユーザーとしてログインする
   def log_in_as(user)
     session[:user_id] = user.id
