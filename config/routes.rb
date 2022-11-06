4184c3880f9ae680ec92a52f9a106efefec53e1d
suzutuki <61527476+suzutuki@users.noreply.github.com>
2022-11-06 20:09:34 +0900

    コードフォーマットを適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  resources :hiits
-  root 'users#index'
-  resources :its do
-    collection do
-      delete 'destroy_all'
-    end
-  end
-  resources :macs
-  resources :tasks do
-    collection do
-      delete 'destroy_all'
-    end
-  end
-  resources :users
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/easy_login', to: 'users#easy_login'
-  delete '/logout', to: 'sessions#destroy'
-  post '/tasks/:id/toggle' => 'tasks#toggle'
-  get '/how_to', to: 'users#how_to'
+   resources :hiits
+   root 'users#index'
+   resources :its do
+     collection do
+       delete 'destroy_all'
+     end
+   end
+   resources :macs
+   resources :tasks do
+     collection do
+       delete 'destroy_all'
+     end
+   end
+   resources :users
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   get '/easy_login', to: 'users#easy_login'
+   delete '/logout', to: 'sessions#destroy'
+   post '/tasks/:id/toggle' => 'tasks#toggle'
+   get '/how_to', to: 'users#how_to'
 end
