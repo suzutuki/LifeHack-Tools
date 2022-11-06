e15e18dae4ddb5bb83ca8f03bc3c916d72178001
suzutuki <61527476+suzutuki@users.noreply.github.com>
2022-11-06 20:18:58 +0900

    コメントを日本語化

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,21 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# Gemfile に記載されている gems を必要とする。
+# :test, :development, :production のいずれかに限定したgemsを含む、Gemfileに記載されているgemsを必要とします。
 Bundler.require(*Rails.groups)
 
 module Todo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    # 元々生成されたRailsのバージョンに合わせた設定のデフォルトを初期化する。
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.action_controller.action_on_unpermitted_parameters = :raise
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # config/environments/* の設定は、ここで指定されたものよりも優先されます。
+    # アプリケーションの設定は config/initializers にあるファイルに入れるべきです。
+    # -- そのディレクトリにある全ての .rb ファイルが自動的にロードされます。
   end
 end
